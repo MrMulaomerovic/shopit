143beac899ae669979661de2d33c8a7d7cf4fc2e
Adnan Mulaomerovic <mrmulaomerovic@gmail.com>
2021-04-04 23:10:38 +0200

    password url

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,21 +2,22 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async options => {
     const transporter = nodemailer.createTransport({
-        host: proccess.env.SMTP_HOST,
-        port: proccess.env.SMTP_PORT,
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
         auth: {
-          user: proccess.env.SMTP_EMAIL,
-          pass: proccess.env.SMTP_PASSWORD
+          user: process.env.SMTP_EMAIL,
+          pass: process.env.SMTP_PASSWORD
         }
       });
 
       const message = {
-          from: `${proccess.env.SMTP_FROM_NAME} <${proccess.env.SMTP_FROM_EMAIL}>`,
+          from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
           to: options.email,
           subject: options.subject,
           text: options.message
       }
+
       await transporter.sendMail(message)
 }
 
-module.exports = sendEmail
+module.exports = sendEmail;
