f7250232e06e35e2ad6e7469660afcac8e5de165
Adnan Mulaomerovic <mrmulaomerovic@gmail.com>
2021-05-26 22:46:01 +0200

    Admin

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,9 +8,15 @@ const {
     forgotPassword,
     getUserProfile,
     updatePassword, 
-    resetPassword } = require('../controllers/authController');
+    resetPassword,
+    updateProfile,
+    allUsers,
+    getUserDetails,
+    updateUser,
+    deleteUser
+        } = require('../controllers/authController');
 
-    const { isAuthenticatedUser } = require('../middlewares/auth')
+    const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
@@ -22,6 +28,14 @@ router.route('/logout').get(logout);
 
 router.route('/me').get(isAuthenticatedUser, getUserProfile);
 router.route('/password/update').put(isAuthenticatedUser,updatePassword);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile)
+
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers)
+router.route('/admin/user/:id')
+        .get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
+        .put(isAuthenticatedUser, authorizeRoles('admin'), updateUser)
+        .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser)
+
 
 
 module.exports = router;
